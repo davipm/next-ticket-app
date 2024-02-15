4cb6c2b8c04ad88bbf41daeff8e19186f58766a8
Davi Pereira <davi.p.m94@gmail.com>
2024-02-15 13:51:43 -0300

    adding nav

---
+++ components/nav.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function Nav() {
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <FontAwesomeIcon
+            icon={faHome}
+            className="text-default-text text-xl"
+          />
+        </Link>
+        <Link href="/ticket/new">
+          <FontAwesomeIcon
+            icon={faTicket}
+            className="text-default-text text-xl"
+          />
+        </Link>
+      </div>
+
+      <div>
+        <p className="text-default-text">davi.p.m94@gmail.com</p>
+      </div>
+    </nav>
+  );
+}
