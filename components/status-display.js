a02310a0d01cca5d8158908a29d9afcfdb088d64
Davi Pereira <davi.p.m94@gmail.com>
2024-02-18 14:59:36 -0300

    refactoring form component

--- components/status-display.js
+++ components/status-display.js
@@ -1,4 +1,5 @@
 import { useCallback } from "react";
+import { cn } from "@/lib/utils";
 
 export function StatusDisplay({ status }) {
   const getColor = useCallback((status) => {
@@ -16,9 +17,10 @@ export function StatusDisplay({ status }) {
 
   return (
     <span
-      className={`inline-block  rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
-        status,
-      )}`}
+      className={cn(
+        "inline-block  rounded-full px-2 py-1 text-xs font-semibold text-gray-700",
+        getColor(status),
+      )}
     >
       {status}
     </span>
