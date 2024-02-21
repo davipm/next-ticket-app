8297b1b6eb6e8efff5c0196b0151d481e20c87ec
Davi Pereira <davi.p.m94@gmail.com>
2024-02-21 13:31:56 -0300

    update spacing

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         <QueryProvider>
           <div className="flex flex-col h-screen max-h-screen">
             <Nav />
-            <div className="flex-grow overflow-y-auto bg-page text-default-text">
+            <div className="flex-grow p-5 overflow-y-auto bg-page text-default-text">
               {children}
             </div>
           </div>
