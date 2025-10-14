0e093eeb7218abb6122ca48680a2241919e5e57e
Davi Pereira <davi.p.m94@gmail.com>
2025-10-14 13:11:32 +0900

    set staleTime for ticket query and adjust prisma paths

--- apps/web/src/components/nav.tsx
+++ apps/web/src/components/nav.tsx
@@ -4,6 +4,7 @@ import { useTickets } from '@/hooks/use-ticket';
 import { orpc } from '@/utils/orpc';
 import { useQuery } from '@tanstack/react-query';
 import { Home, TicketPlus } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export function Nav() {
@@ -21,7 +22,8 @@ export function Nav() {
         </Link>
       </div>
 
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-3">
+        <Image src="/vercel.svg" alt="Logo" width={15} height={15} />
         <p className="text-default-text">davi.p.m94@gmail.com has {total} tickets</p>
         <div className={`h-2 w-2 rounded-full ${checkApi ? 'bg-green-500' : 'bg-red-500'}`} />
       </div>
