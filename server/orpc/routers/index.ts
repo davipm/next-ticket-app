a3609a55e8779caa0a6e563dbb7927d170cd28ad
Davi Pereira <davi.p.m94@gmail.com>
2025-11-07 15:25:43 +0900

    update script

--- server/orpc/routers/index.ts
+++ server/orpc/routers/index.ts
@@ -1,11 +1,11 @@
+import type { RouterClient } from '@orpc/server';
 import { publicProcedure } from '@/server/orpc';
-import type { RouterClient } from "@orpc/server";
 import { ticketRouter } from '@/server/orpc/routers/ticket';
 
 export const appRouter = {
   ticket: ticketRouter,
   healthCheck: publicProcedure.handler(() => {
-    return "OK";
+    return 'OK';
   }),
 };
 
